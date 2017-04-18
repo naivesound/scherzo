83f3bff60b93d5e4059db81b14c576cc7a39624c
Anna Kruglik <kruglik.anna@gmail.com>
2017-04-18 22:47:41 +0300

    moved service binder instance to the application context

--- android/app/src/main/java/com/naivesound/scherzo/MainActivity.java
+++ android/app/src/main/java/com/naivesound/scherzo/MainActivity.java
@@ -2,69 +2,20 @@ package com.naivesound.scherzo;
 
 import android.Manifest;
 import android.app.Activity;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.IBinder;
-import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
 
-    private static final int PERMISSION_REQEUST_ID = 1000;
     private static final String TAG = "MainActivity";
-    private ScherzoService mService;
+    private static final int PERMISSION_REQEUST_ID = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, ScherzoService.class);
-        startService(intent);
-        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
-
-        LinearLayout layout = new LinearLayout(this);
-        layout.setOrientation(LinearLayout.VERTICAL);
-
-        Button bpmButton = new Button(this);
-        bpmButton.setText("Tap");
-        bpmButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                if (mService != null) {
-                    mService.tap();
-                }
-            }
-        });
-        layout.addView(bpmButton);
-
-        Button looperButton = new Button(this);
-        looperButton.setText("Looper");
-        looperButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                if (mService != null) {
-                    mService.loop();
-                }
-            }
-        });
-        layout.addView(looperButton);
-
-        Button looperExtButton = new Button(this);
-        looperExtButton.setText("Cancel");
-        looperExtButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                if (mService != null) {
-                    mService.cancelLoop();
-                }
-            }
-        });
-        layout.addView(looperExtButton);
-
-        setContentView(layout);
+        setContentView(new ScherzoView(this));
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (checkSelfPermission(
@@ -77,9 +28,7 @@ public class MainActivity extends Activity {
         }
     }
 
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[],
-                                           int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
         case PERMISSION_REQEUST_ID:
             //
@@ -90,30 +39,8 @@ public class MainActivity extends Activity {
     @Override
     protected void onDestroy() {
         if (isFinishing()) {
-            if (mService != null) {
-                mService.destroy();
-                unbindService(mConnection);
-                mService = null;
-            }
-            Intent intent = new Intent(this, ScherzoService.class);
-            stopService(intent);
+            App.stopService();
         }
         super.onDestroy();
     }
-
-    private ServiceConnection mConnection = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName className,
-                                       IBinder service) {
-            ScherzoService.ScherzoBinder binder =
-                (ScherzoService.ScherzoBinder)service;
-            mService = binder.getService();
-            mService.handleMIDI();
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName className) {
-            mService = null;
-        }
-    };
 }
