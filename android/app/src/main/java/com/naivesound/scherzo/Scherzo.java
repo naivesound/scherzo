b1910eaf150221456c3d67770ea741d2e6b73b58
Anna Kruglik <kruglik.anna@gmail.com>
2017-04-18 22:50:52 +0300

    fixed noteOff()

--- android/app/src/main/java/com/naivesound/scherzo/Scherzo.java
+++ android/app/src/main/java/com/naivesound/scherzo/Scherzo.java
@@ -15,7 +15,7 @@ public class Scherzo {
     public void noteOn(int chan, int note, int velocity) {
         midi(mRef, 0x90, note, velocity);
     }
-    public void noteOff(int chan, int note) { midi(mRef, 0x80, chan, note); }
+    public void noteOff(int chan, int note) { midi(mRef, 0x80, note, 0); }
     public void tap() { midi(mRef, 0xb0, 0x50, 1); }
     public void loop() { midi(mRef, 0xb0, 0x51, 1); }
     public void cancelLoop() { midi(mRef, 0xb0, 0x52, 1); }
